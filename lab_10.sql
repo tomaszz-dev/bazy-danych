2e76bc255cec129c4df6853d295b91c4079a61f1
Tomasz Zawacki <tomaszzawacki6@gmail.com>
2026-01-13 15:04:28 +0100

    Update lab_10.sql

--- lab_10.sql
+++ lab_10.sql
@@ -11,35 +11,10 @@ CHANGE_TIME datetime default current_timestamp,
 NEW_STATUS_NAME varchar(234)
 );
 
-drop table order_logi;
 
 
-DELIMITER //
-CREATE TRIGGER order_record AFTER UPDATE
-ON `ORDER` 
-FOR EACH ROW
-BEGIN
-    insert into order_logi(ORDER_NUMBER, CHANGE_TIME, NEW_STATUS_NAME)
-    VALUES (NEW.ORDER_NUMBER, now(), NEW.ORDER_STATUS);
-END //
-DELIMITER ;
 
-DROP trigger order_record;
 
-select * from `ORDER`;
-
-UPDATE `ORDER`
-SET ORDER_STATUS = 5
-WHERE EMPLOYEE_ID = 7;
-
-select * from `ORDER`;
-select * from `order_logi`;
-
-select * from ORDER_STATUS;
-
-#####################
-# rozwiazanie zad 1
-#################################
 DELIMITER //
 CREATE TRIGGER status_after_update_2 AFTER UPDATE
 ON `ORDER` 
@@ -52,6 +27,14 @@ BEGIN
 END //
 DELIMITER ;
 
+UPDATE `ORDER`
+SET ORDER_STATUS = 5
+WHERE EMPLOYEE_ID = 7;
+
+select * from `ORDER`;
+select * from `order_logi`;
+
+select * from ORDER_STATUS;
 
 
 
